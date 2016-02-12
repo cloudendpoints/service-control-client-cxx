c558dc4de60fecf9dcff6963851d1702073b54c2
Wayne Zhang <qiwzhang@google.com>
2016-02-12 15:03:19 -0800

    Added operation_aggregator.
    cl/115475060
    
    Change-Id: I920f940971bcb536834c9b528a1c680ab7ab42c0

---
+++ operation_aggregator.h
@@ -0,0 +1,67 @@
+// Internal data structure used to cache and aggregate operations.
+
+#ifndef GOOGLE_SERVICE_CONTROL_CLIENT_OPERATION_AGGREGATOR_H_
+#define GOOGLE_SERVICE_CONTROL_CLIENT_OPERATION_AGGREGATOR_H_
+
+#include <unordered_map>
+
+#include "google/api/metric.pb.h"
+#include "google/api/servicecontrol/v1/metric_value.pb.h"
+#include "google/api/servicecontrol/v1/operation.pb.h"
+#include "google_macros.h"
+
+namespace google {
+namespace service_control_client {
+
+// Thread compatible.
+class OperationAggregator {
+ public:
+  // Constructor. Does not take ownership of metric_kinds, which must outlive
+  // this instance.
+  OperationAggregator(
+      const ::google::api::servicecontrol::v1::Operation& operation,
+      const std::unordered_map<std::string,
+                               ::google::api::MetricDescriptor::MetricKind>*
+          metric_kinds);
+
+  // Merges the given operation with this operation, assuming the given
+  // operation has the same operation signature.
+  void MergeOperation(
+      const ::google::api::servicecontrol::v1::Operation& operation);
+
+  // Transforms to Operation proto message.
+  ::google::api::servicecontrol::v1::Operation ToOperationProto() const;
+
+ private:
+  // Merges the metric value sets in the given operation into this operation.
+  void MergeMetricValueSets(
+      const ::google::api::servicecontrol::v1::Operation& operation);
+
+  // Merges the log entries in the given operation into this operation.
+  void MergeLogEntries(
+      const ::google::api::servicecontrol::v1::Operation& operation);
+
+  // Used to store everything but metric value sets.
+  ::google::api::servicecontrol::v1::Operation operation_;
+
+  // Aggregated metric values in the operation.
+  // Key is metric_name.
+  // Value is a map of metric value signature to aggregated metric value.
+  std::unordered_map<
+      std::string,
+      std::unordered_map<std::string,
+                         ::google::api::servicecontrol::v1::MetricValue>>
+      metric_value_sets_;
+
+  // Metric kinds. Key is the metric name and value is the metric kind.
+  // Defaults to DELTA if not specified.
+  const std::unordered_map<
+      std::string, ::google::api::MetricDescriptor::MetricKind>* metric_kinds_;
+
+  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(OperationAggregator);
+};
+
+}  // namespace service_control_client
+}  // namespace google
+
+#endif  // GOOGLE_SERVICE_CONTROL_CLIENT_OPERATION_AGGREGATOR_H_
