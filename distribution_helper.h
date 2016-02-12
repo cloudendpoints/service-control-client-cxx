4d9155f1c08a04169b8ec3ad455b8780eeb6349f
Wayne Zhang <qiwzhang@google.com>
2016-02-11 16:35:24 -0800

    Added distribution_helper.
    Change-Id: Id7ec58527fa5d7c8e648ac331bf2c588f2fc927b

---
+++ distribution_helper.h
@@ -0,0 +1,45 @@
+#ifndef GOOGLE_SERVICE_CONTROL_CLIENT_DISTRIBUTION_HELPER_H_
+#define GOOGLE_SERVICE_CONTROL_CLIENT_DISTRIBUTION_HELPER_H_
+
+#include "google/api/servicecontrol/v1/distribution.pb.h"
+#include "google/protobuf/stubs/status.h"
+
+namespace google {
+namespace service_control_client {
+
+// A helper class for handling Distribution proto message.
+// Thread safe.
+class DistributionHelper final {
+ public:
+  // Inits the distribution with exponential buckets.
+  static ::google::protobuf::util::Status InitExponential(
+      int num_finite_buckets, double growth_factor, double scale,
+      ::google::api::servicecontrol::v1::Distribution* distribution);
+
+  // Inits the distribution with linear buckets.
+  static ::google::protobuf::util::Status InitLinear(
+      int num_finite_buckets, double width, double offset,
+      ::google::api::servicecontrol::v1::Distribution* distribution);
+
+  // Inits the distribution with explicit buckets. Note that bounds must be
+  // sorted in ascending order and contain no duplicates.
+  static ::google::protobuf::util::Status InitExplicit(
+      const std::vector<double>& bounds,
+      ::google::api::servicecontrol::v1::Distribution* distribution);
+
+  // Adds one more sample to the given distribution.
+  static ::google::protobuf::util::Status AddSample(
+      double value,
+      ::google::api::servicecontrol::v1::Distribution* distribution);
+
+  // Merges the "from" distribution to "to" distribution.
+  // No change if the bucket options does not match.
+  static ::google::protobuf::util::Status Merge(
+      const ::google::api::servicecontrol::v1::Distribution& from,
+      ::google::api::servicecontrol::v1::Distribution* to);
+};
+
+}  // namespace service_control_client
+}  // namespace google
+
+#endif  // GOOGLE_SERVICE_CONTROL_CLIENT_DISTRIBUTION_HELPER_H_
