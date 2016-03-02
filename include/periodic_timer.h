7441f64c6ab45f70c635693d2721ab0720339da9
Wayne Zhang <qiwzhang@google.com>
2016-03-02 14:32:46 -0800

    Added high client level library
    Change-Id: I08495c8758513b0006c36c84d74f5e4ddac18e98

---
+++ include/periodic_timer.h
@@ -0,0 +1,41 @@
+#ifndef GOOGLE_SERVICE_CONTROL_CLIENT_PERIODIC_TIMER_H_
+#define GOOGLE_SERVICE_CONTROL_CLIENT_PERIODIC_TIMER_H_
+
+#include <functional>
+
+namespace google {
+namespace service_control_client {
+
+// An interface to provide periodic timer for the library.
+class PeriodicTimer {
+ public:
+  // Defines the function to be called periodically by the timer.
+  // timer_is_canceled indicates if the function is called when the timer
+  // is being canceled. It should be true for all calls except the last
+  // one when the timer is being canceled.
+  using PeriodicCallback = std::function<void(bool timer_is_canceled)>;
+
+  // Represents a timer created by StartTimer.
+  // Its only purpose is to cancel the timer instance.
+  class Timer {
+    // Destructor
+    virtual ~Timer() {}
+
+    // Cancels the timer.
+    virtual void Cancel() = 0;
+  };
+
+  // Destructor
+  virtual ~PeriodicTimer() {}
+
+  // Creates a periodic timer periodically calling the callback
+  // with desired interval. The returned object can be used to cancel
+  // the instance.
+  virtual std::unique_ptr<Timer> StartTimer(
+      int interval_ms, PeriodicCallback callback) = 0;
+};
+
+}  // namespace service_control_client
+}  // namespace google
+
+#endif  // GOOGLE_SERVICE_CONTROL_CLIENT_PERIODIC_TIMER_H_
