9f55d962c816a2fc84e61911a3da157bedf117c0
Qian Sun <qians@google.com>
2016-03-31 16:09:23 -0700

    Cleared paths in header files.
    Change-Id: I54011f303cc7a842e5ff30efc0ba944ac40e3a7b

--- include/service_control_client.h
+++ include/service_control_client.h
@@ -7,9 +7,13 @@
 
 #include "google/api/servicecontrol/v1/service_controller.pb.h"
 #include "google/protobuf/stubs/status.h"
-#include "include/aggregation_options.h"
-#include "include/periodic_timer.h"
-#include "include/transport.h"
+// To make it easier for other packages to include this module as their
+// submodule, following include rules have to be followed:
+// A public exposed header can only include local headers in the same folder.
+// When including, not to put folder name in the include, just the file name.
+#include "aggregation_options.h"
+#include "periodic_timer.h"
+#include "transport.h"
 
 namespace google {
 namespace service_control_client {
