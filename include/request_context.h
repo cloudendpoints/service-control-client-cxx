adccfce82b4db9ce035f7acb185fa12b4e0e296e
Qian Sun <qians@google.com>
2016-04-01 10:00:10 -0700

    Added RequestContext to distinguish subrequest from  detached request.
    Change-Id: I68514258e36b5e46c8799d69b8723d99d002c068

---
+++ include/request_context.h
@@ -0,0 +1,14 @@
+#ifndef GOOGLE_SERVICE_CONTROL_CLIENT_CXX_REQUEST_CONTEXT_H_
+#define GOOGLE_SERVICE_CONTROL_CLIENT_CXX_REQUEST_CONTEXT_H_
+
+namespace google {
+namespace service_control_client {
+// Represents a RequestContext.
+class RequestContext {
+ public:
+  virtual ~RequestContext() {}
+};
+}  // namespace service_control_client
+}  // namespace google
+
+#endif  // SERVICE_CONTROL_CLIENT_CXX_REQUEST_CONTEXT_H
